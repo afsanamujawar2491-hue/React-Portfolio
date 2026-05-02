82ba57e965c4db5f24733b8ec4519f231c92d328
Afsanamujawar <afsanamujawar2491@gmail.com>
2026-05-03 00:29:33 +0530

    Resumeupload changes

--- src/Resume.js
+++ src/Resume.js
@@ -1,11 +1,17 @@
-
 import React from 'react';
 
 const Resume = () => (
   <div style={{ textAlign: 'center' }}>
     <h2>Resume</h2>
     <p>Download my resume for detailed experience and skills.</p>
-    <a className="button" href="/resume.pdf" target="_blank">Download Resume</a>
+
+    <a 
+      href="/Resume – Afsana Mujawar.pdf" 
+      download="Resume – Afsana Mujawar.pdf"
+      className="button"
+    >
+      Download Resume
+    </a>
   </div>
 );
 
